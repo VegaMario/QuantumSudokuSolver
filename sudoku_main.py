48960d069d8f5c9da3248bf15b432fc3dcbb8cbf
VegaMario <vegam5954@uhcl.edu>
2021-07-19 12:15:43 -0500

    some comments

--- sudoku_main.py
+++ sudoku_main.py
@@ -18,14 +18,21 @@ def generate(dim, nsize, blanks):
 # solve a sudoku puzzle
 def solve(filename, qubotype, samplername, numreads, plot):
     dim = 1
+    qtype = ''
+    if qubotype:
+        qtype = 'complex'
+        print(qtype)
+    else:
+        qtype = 'simple'
+        print(qtype)
     for i in filename:
         if i == 'x':
             dim += 1
     print("----------{}D Solver----------".format(dim))
     if dim == 2:
-        nxn_solver.solve_nxn(filename + ".txt", qubotype, samplername, numreads, plot)
+        nxn_solver.solve_nxn(filename + ".txt", qtype, samplername, numreads, plot)
     elif dim == 3:
-        nxnxn_solver.solve_nxnxn(filename + ".txt", qubotype, samplername, numreads, plot)
+        nxnxn_solver.solve_nxnxn(filename + ".txt", qtype, samplername, numreads, plot)
     else:
         print("Invalid")
     dim = 1
@@ -38,8 +45,8 @@ def main():
     filename = sys.argv[2]
 
     if operation == 'solve':
-        QUBO_type = input('QUBO Type (complex or simple): ').lower()
-        make_plot = int(input('Make Plot? (1 or 0): '))
+        QUBO_type = int(input('QUBO Type? (1 for complex, 0 for simple): '))
+        make_plot = int(input('Make Plot? (1 for yes, 0 for no): '))
         solve(filename, QUBO_type, "neal", 30, make_plot)
     elif operation == 'generate':
         dim = 1
